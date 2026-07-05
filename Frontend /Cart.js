757374a44e5589deaf9c3fa1fccf7cce61c74d29
EVERYDAYPLUG_48HRS <mogau.sebothoma@gmail.com>
2026-07-05 04:23:48 +0200

    Implement basic shopping cart functionality

--- Frontend /Cart.js
+++ Frontend /Cart.js
@@ -1,1 +1,15 @@
+// ======================================
+// SHOPPING CART
+// ======================================
 
+let cart = [];
+
+function addToCart(product){
+
+    cart.push(product);
+
+    console.log(cart);
+
+}
+
+console.log("Shopping Cart Ready");
